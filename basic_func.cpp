934daf91528e434a0265a9cd52505659705e05c6
unknown <cjy>
2026-02-10 22:07:32 +0800

    添加查看机器各个类型占据字节以及判断大小端函数

--- basic_func.cpp
+++ basic_func.cpp
@@ -5,7 +5,7 @@
 #include <vector>
 using namespace std;
 
-
+/* 计算字符串的长度 */
 int my_strlen(const char *str){  // input string`s address
     if(str == nullptr)
         return 0;
@@ -17,6 +17,7 @@ int my_strlen(const char *str){  // input string`s address
 }
 
 
+/* 内存拷贝函数 */
 void* my_memcopy(char *des, char *src, size_t size){
     char *ptr_d = des;
     char *ptr_s = src;
@@ -27,7 +28,7 @@ void* my_memcopy(char *des, char *src, size_t size){
     return des;
 }
 
-
+/* 内存设置函数 */
 void my_memset(char *des, char value, size_t size){
     char *ptr = des;
     while(size > 0){
@@ -36,14 +37,43 @@ void my_memset(char *des, char value, size_t size){
     }
 }
 
-
+/* 偏移量计算宏 */
 #define my_offestof(type, member) (size_t) ( &( ((type*) 0)->member) )
 typedef struct {
-    char  a;      // 0
-    int   b;      // 4 (通常有3字节填充)
-    short c;      // 8
-    char  d;      // 10
+    char  a;
+    int   b;
+    short c;
+    char  d;
 } MyStruct;
+
+
+/* 查看大端还是小端 */
+void check_endianness(){
+    union{
+        int a = 0x1234;
+        char c;
+    } endianness;
+
+    if(endianness.c == 0x34)
+        printf("current platform is little-endian\n");
+    else
+        printf("current platform is big-endian\n");
+}
+
+
+/* 查看当前机器各个类型的字节数 */
+void check_type_size(){
+    printf("sizeof(char) = %llu\n", sizeof(char));
+    printf("sizeof(short) = %llu\n", sizeof(short));
+    printf("sizeof(int) = %llu\n", sizeof(int));    
+    printf("sizeof(long) = %llu\n", sizeof(long));
+    printf("sizeof(long long) = %llu\n", sizeof(long long));
+    printf("sizeof(float) = %llu\n", sizeof(float));
+    printf("sizeof(double) = %llu\n", sizeof(double));
+
+}
+
+
 
 int main(){
     char src[] = "hello world";
@@ -54,5 +84,7 @@ int main(){
     size_t offest = my_offestof(MyStruct, d);
     printf("Test 2: offest of b is %llu\n", offest);
 
+    check_endianness();
+    check_type_size();
     return 0;
 }
